188db7b3212d9195470f095dd356cd92994a6178
Donato <donato.ferraro@ictandmore.it>
2016-07-01 18:06:25 +0200

    commit debugger

--- JustJava/app/src/main/java/it/jaxma/com/justjava/MainActivity.java
+++ JustJava/app/src/main/java/it/jaxma/com/justjava/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +19,26 @@ public class MainActivity extends AppCompatActivity {
     /*
     * Questo metodo è chiamato quando clicco sul botton order*/
 
+
+    public void increment(View view){
+
+        int quantity = 3;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    public void decrement(View view){
+        int quantity = 1;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+
     public void submitOrder(View view) {
 
-        display(1);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
 
@@ -27,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
 
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
 }
